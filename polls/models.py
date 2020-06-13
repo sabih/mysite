d164f9481832a644fc812e2a1bec1ce79fade765
Sabih Ahmad Khan <sabihahmadkhan@gmail.com>
2020-06-13 13:19:04 +0400

    Completed Django 3.0 tutorial with codes from Tutorial 01 to 07
    https://docs.djangoproject.com/en/3.0/intro/tutorial07/

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,6 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 
 
 class Question(models.Model):
@@ -13,7 +12,14 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
